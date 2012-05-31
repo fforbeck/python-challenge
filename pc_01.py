76569b85ba61628dc18430f6d5871835068a84e9
Felipe Forbeck <fforbeck@gmail.com>
2012-05-30 23:38:46 -0300

    refactory pc 1

--- pc_01.py
+++ pc_01.py
@@ -15,15 +15,7 @@ rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb
 gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. 
 sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj."""
 
-print("===========  Tradução ============ \n")
+
 print( texto.translate(str.maketrans(alfabeto, chave)))
-print("===========  ======== ============ \n")
 
-
-
-"""i hope you didnt translate it by hand.
-thats what computers are for. doing it in by hand 
-is inefficient and that's why this text is so long. 
-using string.maketrans() is recommended. now apply on the url."""
-
-print( "map".translate(str.maketrans(alfabeto, chave)))
+print( "secret word: " + "map".translate(str.maketrans(alfabeto, chave)))
