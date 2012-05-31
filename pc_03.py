afd59558dbf4fb9e94bee74c9b11427b5cb3beb7
Felipe Forbeck <fforbeck@gmail.com>
2012-05-30 23:34:58 -0300

    solução pc 3

--- pc_03.py
+++ pc_03.py
@@ -1,18 +1,7 @@
 import re
-import itertools
 
-especial_letters = set()
+_file = open("pc_03.txt", "rt")
+data = _file.read()
+_file.close()
 
-in_file = open('pc_03.txt', "rt")
-while True:
-	line = in_file.readline()
-	if not line:
-		break
-	
-	match = re.search('[A-Z]{3}[a-z]{1}[A-Z]{3}', line)
-	if match is not None:
-		especial_letters.add(match.group()[3])
-	    
-in_file.close()
-
-print(''.join(sorted(especial_letters)))
+print("".join(re.findall("[^A-Z]+[A-Z]{3}([a-z])[A-Z]{3}[^A-Z]+", data)))
